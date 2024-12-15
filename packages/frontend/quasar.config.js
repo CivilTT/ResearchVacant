5c8b52db50f9bcd1ef236816120e5e4b496d5a33
CivilTT <89191801+CivilTT@users.noreply.github.com>
2024-12-15 12:41:50 +0900

    Set publicPath in `quasar.config.js`

--- packages/frontend/quasar.config.js
+++ packages/frontend/quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/ResearchVacant'
       // analyze: true,
       // env: {},
       // rawDefine: {}
