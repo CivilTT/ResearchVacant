8c514caf0ee86db520127baccadd0229e8fdec7d
CivilTT <89191801+CivilTT@users.noreply.github.com>
2024-12-15 12:04:00 +0900

    Removed viteSingleFile setting in `quasar.config.js`

--- packages/frontend/quasar.config.js
+++ packages/frontend/quasar.config.js
@@ -76,7 +76,7 @@ module.exports = configure(function (/* ctx */) {
           },
           { server: false },
         ],
-        [viteSingleFile, {}],
+        // [viteSingleFile, {}],
       ],
     },
 
